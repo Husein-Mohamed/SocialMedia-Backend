62b99a94cbfcbd0c90c49df6b7992a9e894e6cd4
Hussein <helnemar@yahoo.com>
2023-06-05 07:34:15 +0300

    post, comment

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const {createPost, getAllPosts, getPostById, updatePostById, deletePostById} = require('../controllers/postController')
 const router = express.Router();
+
+router.post('/', createPost)
+router.get('/', getAllPosts)
+router.get('/:id', getPostById)
+router.patch('/:id', updatePostById)
+router.delete('/:id', deletePostById)
 
 module.exports = router
