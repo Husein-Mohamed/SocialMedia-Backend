acd6c16cf4e39817e629ec2d40cc8b2337ddbd1a
Aya zain <ayaashrafzain@gmail.com>
2023-06-06 02:24:59 +0300

    admin CRUD operation

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,14 +33,41 @@ const updateUser = async (req, res, next) => {
 const getAllUsers =async (req, res, next) => {
   if (!req.Admin)
   return next (new AppError("You are not Admin", 404) );
-  let users = await User.find().select(+"password");
+  let users = await User.find();
   res.send(users);
 };
-
 const deleteUser = async (req, res, next) => {
  const user = req.user;
   await User.findByIdAndDelete({_id:user._id})
   res.send('User deleted Successfully !!')
 };
+const deleteUserByEmail = async (req, res, next) => {
+  const user = req.user;
+  if(!req.Admin)
+  return next (new AppError("You are not Admin", 404) ); 
+  const {email} = req.body ; 
+  await User.findOneAndDelete({email:email})
+   res.send('User deleted Successfully !!')
+ };
+ const getUserByEmail = async (req, res, next) => {
+  const {email} = req.body ; 
+ let user =  await User.findOne({email:email})
+   res.send(user)
+ };
 
-module.exports = { getAllUsers, register, login, updateUser , deleteUser };
+
+  const addUser = async (req, res, next) => {
+    const user = req.user;
+    if(!req.Admin)
+    return next (new AppError("You are not Admin", 404) ); 
+  const newUser = await User.create(req.body);
+  user.passward = undefined;
+  var token = jwt.sign({ id: user._id }, process.env.SECRET_KEY_JWT);
+  res.send({ user, token: token });
+};
+const getUserData = async (req, res, next) => {
+  req.user.passward =undefined;
+  res.send(req.user);
+};
+
+module.exports = { getAllUsers, register, login, updateUser , deleteUser  , getUserData , deleteUserByEmail, addUser , getUserByEmail};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const {getAllUsers, register , login ,  updateUser  ,deleteUser} = require('../controllers/userController');
+const {getAllUsers, register , login ,  updateUser  ,deleteUser , getUserData , deleteUserByEmail , addUser, getUserByEmail} = require('../controllers/userController');
 const {uploadToMulter, uploadPP} = require('../utils/multerConfig');
-const {signUpValidtion , logInValidtion , updateValidtion } = require('../utils/Validation');
+const {signUpValidtion , logInValidtion , updateValidtion ,findByEmailValidtion} = require('../utils/Validation');
 const verifyToken = require('../utils/VerifyToken');
 
 router.post('/register', signUpValidtion , register);
 router.post('/login', logInValidtion , login);
 router.post('/upload',verifyToken,uploadToMulter ,uploadPP )
 router.patch('/update' ,updateValidtion ,verifyToken,updateUser )
-router.delete('/delete' , verifyToken , deleteUser )
+router.delete('/' , verifyToken , deleteUser )
+router.get ('/',verifyToken ,getUserData)
+router.get('/getuserdata' , verifyToken , findByEmailValidtion,getUserByEmail)
 
 
-router.post('/admin/register', signUpValidtion , register);
+router.post('/admin', signUpValidtion , verifyToken , addUser);
 router.post('/admin/login', logInValidtion , login);
 router.get('/admin',verifyToken ,getAllUsers); 
+router.delete('/admin' , verifyToken ,findByEmailValidtion ,deleteUserByEmail )
 
 
 module.exports = router 
